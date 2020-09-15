a93014d88bc259423fc5145a4f2e405f459a176d
Emil Edström <ge97xar@mytum.de>
2020-09-15 10:58:30 +0200

    Added Imread function

--- main.m
+++ main.m
@@ -1,31 +1,31 @@
-
 %% bildserie 1
-im1 = imread('HDR/Img1.tiff');
-im2 = imread('HDR/Img2.tiff');
-im3 = imread('HDR/Img3.tiff');
-im4 = imread('HDR/Img4.tiff');
-im5 = imread('HDR/Img5.tiff');
-im6 = imread('HDR/Img6.tiff');
-im7 = imread('HDR/Img7.tiff');
-im8 = imread('HDR/Img8.tiff');
-im9 = imread('HDR/Img9.tiff');
-im10 = imread('HDR/Img10.tiff');
-im11 = imread('HDR/Img11.tiff');
-im12 = imread('HDR/Img12.tiff');
-im13 = imread('HDR/Img13.tiff');
-im14 = imread('HDR/Img14.tiff');
 
+% Read in pictures from folder
+HDRbilderna = 'HDR/';
 
+if ~isfolder(HDRbilderna)
+  errorMessage = sprintf('Error: The following folder does not exist:\n%s', HDRbilderna);
+  uiwait(warndlg(errorMessage));
+  return;
+end
 
-images = {im1, im2, im3, im4,im5, im6, im7, im8, im9, im10, im11, im12, im13, im14};
-montage(images)
+filePattern = fullfile(HDRbilderna, '*.tiff');
+tiffFiles = dir(filePattern);
+for i = 1:length(tiffFiles)
+  firstLetters = strcat('Img',num2str(i),'.tiff');
+  completeName = fullfile(HDRbilderna, firstLetters);
+  im{i} = imread(completeName);
+  images{i} = im{i};
+end
+
+%montage(images);
 
 exposures = {1/4000, 1/3000, 1/2000, 1/1000, 1/500, 1/250, 1/125, 1/60, 1/30, 1/15, 1/6, 1/3, 2/3, 4/3};
 
 
 %% HDR solver
 
-[R,G,B] = HDRSolver(images, exposures);
+%[R,G,B] = HDRSolver(images, exposures);
 
 
 %% sample
